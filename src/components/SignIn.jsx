17482530b1743e27a988e30ada87be31ab5e2bb3
SandaruwanB <sandarusbandara110@gmail.com>
2023-08-14 23:19:04 +0530

    DEV sign in page

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { LineWave } from 'react-loader-spinner'
-import {  Dropdown, Space, Row, Col } from 'antd'
-import { FaUser, FaCaretDown, FaCopyright } from 'react-icons/fa'
+import {  Dropdown, Space, Row, Col, Divider, Modal } from 'antd'
+import { FaUser, FaCaretDown, FaCopyright, FaUserCog, FaLock, FaEye, FaEyeSlash } from 'react-icons/fa'
 
+
+const showModal = ()=>{
+  Modal.info({
+    title : "Oopzz Something have to tell",
+    content : (
+      <p>This option is not available yet in our web.Please use android app instead.</p>
+    )
+  })
+}
 
 function SignIn() {
   const [loading, setLoading] = useState(true);
+  const [isPassword, setIsPassword] = useState(true);
 
   useEffect(()=>{
     setTimeout(()=>{
@@ -17,6 +27,7 @@ function SignIn() {
     {
       label : "Sign Up",
       key : 0,
+      onclick : ()=>showModal(),
     },
     {
       type : "divider"
@@ -65,21 +76,47 @@ function SignIn() {
           </div>
           <div className='mid'>
             <Row>
+              <Col xs={0} sm={0} md={1} lg={1}/>
               <Col xs={24} sm={24} md={9} lg={9} className='midCont'>
                 <img src="/images/bg1.png" alt="bg img" className='bgimgHome'/>
               </Col>
-              <Col xs={24} sm={24} md={7} lg={7}  className='midCont2'>
+              <Col xs={24} sm={24} md={5} lg={5}  className='midCont2'>
                 <h2>WELCOME BACK TO EDUPRO</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit ducimus facere aliquid dolor eaque. Rerum non ad error accusantium recusandae ullam, aperiam pariatur, perferendis saepe debitis, fuga laboriosam corrupti tenetur?<br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto perspiciatis nostrum nobis ex incidunt cupiditate voluptas reprehenderit consequuntur consectetur vitae in neque ratione temporibus rerum, veritatis aperiam itaque beatae a. <br /><br />Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem libero illo quia necessitatibus vitae tempora debitis tempore provident distinctio ea blanditiis earum deleniti accusamus nostrum dolorem veritatis nemo, ab quaerat!</p>
-              </Col>
+                </Col>
               <Col xs={24} sm={24} md={8} lg={8} className='midCont'>
                 <div className='formcontent'>
                   <h1>Sign In</h1>
                   <div className="form">
-                    
+                    <div className="username">
+                      <label htmlFor="username">User Name</label>
+                      <input type="text" id='username' placeholder='User Name' />
+                      <FaUserCog className='leftIcon'/>
+                    </div>
+                    <div className="password">
+                      <label htmlFor="password">Password</label>
+                      <input type={isPassword ? "password" : "text"} name="password" id="password" placeholder='Password'/>
+                      <FaLock className='leftIcon' style={{fontSize: '1rem'}}/>
+                      {isPassword ? 
+                      <FaEye className='eyeIcon' onClick={()=>setIsPassword(false)}/>
+                      :
+                      <FaEyeSlash className='eyeIcon' onClick={()=>setIsPassword(true)}/>
+                      }
+                    </div>
+                    <div style={{width : '78%', marginLeft : '12%', marginTop: '20px',textAlign : 'right',}}>
+                      <a href="/forgot" style={{display : 'inline-block', }}>Forgot Password?</a>
+                    </div>
+                    <div className='buttonlogin'>
+                      <button>Sign In</button>
+                    </div>
+                    <div style={{width : '80%', marginLeft : '10%', marginTop : 50}}>
+                      <Divider plain>Or</Divider>
+                      <p style={{textAlign : 'center', cursor : 'pointer', color : '#1677ff'}} onClick={()=>showModal()}>Create New Account</p>
+                    </div>
                   </div>
                 </div>
               </Col>
+              <Col xs={0} sm={0} md={1} lg={1}/>
             </Row>
           </div>
           <div className="bottom">
