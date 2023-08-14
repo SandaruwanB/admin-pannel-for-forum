6bb2fd49c83e4a7d18fa524a86bf998bf0b0d2b3
SandaruwanB <sandarusbandara110@gmail.com>
2023-08-14 08:48:44 +0530

    INS andtd

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { LineWave } from 'react-loader-spinner'
 
 function SignIn() {
   return (
-    <div></div>
+    <div>
+      <div>
+        <LineWave
+          height="150"
+          width="150"
+          radius="16"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
+        <div className='test'>test text</div>
+      </div>
+    </div>
   )
 }
 
