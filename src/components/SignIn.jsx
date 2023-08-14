09e5566998ad11cc8d3eab933d069360490905fb
SandaruwanB <sandarusbandara110@gmail.com>
2023-08-14 09:45:14 +0530

    DEV loading spinner

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { LineWave } from 'react-loader-spinner'
+import { Button } from 'antd'
+
+
 
 function SignIn() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false);
+    }, 2000)
+  },[setLoading]);
+
+  
   return (
     <div>
       <div>
-        <LineWave
-          height="150"
-          width="150"
-          radius="16"
-          color="green"
-          ariaLabel="loading"
-          wrapperStyle
-          wrapperClass
-        />
+        {loading ? 
+        <div className='loader'>
+          <LineWave
+            height="150"
+            width="150"
+            radius="16"
+            color="#091C7A"
+            ariaLabel="loading"
+            wrapperStyle
+            wrapperClass
+          />
+        </div>
+
+        :
+        <div>
         <div className='test'>test text</div>
+        <Button type='primary' className='button'>button</Button>
+        </div>
+
+      }
       </div>
     </div>
   )
