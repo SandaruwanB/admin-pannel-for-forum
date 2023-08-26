d04b179d0efbda11ab56a8cf72aff0a15d6e5401
SandaruwanB <sandarusbandara110@gmail.com>
2023-08-26 23:19:43 +0530

    DEV user control

--- src/components/admin/users.jsx
+++ src/components/admin/users.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Layout } from 'antd'
+import SideNav from './layouts/sideNav'
+import TopNav from './layouts/topNav'
+const { Content } = Layout
 
 function Users() {
   return (
-    <div>Users</div>
+    <Layout>
+        <SideNav key={1}/>
+        <Layout>
+            <TopNav />
+            <Content
+                style={{
+                    margin: '24px 16px 0',
+                }}
+            >
+            <Layout
+                style={{
+                padding: 24,
+                minHeight: '60vh',
+                background: "yellow",
+                }}
+            >
+                <Content>
+
+                </Content>
+            </Layout>
+            </Content>
+        </Layout>
+    </Layout>
   )
 }
 
